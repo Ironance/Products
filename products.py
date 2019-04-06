24557ddceb5a82a6be1ea779590924adad64a0c4
Erika Wu <ironance@hotmail.com>
2019-04-06 11:06:07 +0800

    Add skip header by 'continue'

--- products.py
+++ products.py
@@ -1,13 +1,14 @@
 #read the file
-products = [['product', 'price']]
+products = []
 with open('products.csv', 'r') as f:
     for line in f:
+        if 'product,price' in line:
+            continue
         name, price = line.strip().split(',')
-        if name != 'product':
-            products.append([name,price])
-
+        products.append([name,price])
 print(products)
 
+#user input
 while True:
     name = input('Please input the product: ')
     if name =='q':
@@ -16,12 +17,13 @@ while True:
     products.append([name, price])
 print(products)
 
+#print the log
 for p in products:
-    if p[0] != 'product':
-        print('The price of', p[0], 'is', p[1])
+    print('The price of', p[0], 'is', p[1])
 
-
+#write into file
 with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('product, price\n')
     for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
 
