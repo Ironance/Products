4c20dc9b04d28f97e3eb640a7b0616e397e19d80
Erika Wu <ironance@hotmail.com>
2019-04-06 09:56:42 +0800

    Add write data into csv file

--- products.py
+++ products.py
@@ -5,12 +5,14 @@ while True:
     if name =='q':
         break
     price = input('Please input the price: ')
-    # # p = []
-    # # p.append(name)
-    # # p.append(price)
-    # p = [name, price]
     products.append([name, price])
 print(products)
 
 for p in products:
     print('The price of', p[0], 'is', p[1])
+
+
+with open('products.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
+
